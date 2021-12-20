1513237d6747c03ada452477859a930720133bd1
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2021-12-20 16:40:36 +0300

    serit degisimi

--- LaneChanger.m
+++ LaneChanger.m
@@ -65,38 +65,6 @@ classdef LaneChanger < matlab.System
                     obj.a5 = X(6);
                 end
                 
-                if (clock>34) && (isempty(obj.overtake))
-                    obj.overtake= true;
-                    obj.t_i = clock;
-                    
-                    d_i =         [1  obj.t_i   obj.t_i^2   obj.t_i^3    obj.t_i^4    obj.t_i^5]; % -2.5
-                    d_dot_i =     [0  1   2*obj.t_i   3*obj.t_i^2  4*obj.t_i^3  5*obj.t_i^4]; %  0
-                    d_ddot_i =    [0  0   2     6*obj.t_i    12*obj.t_i^2  20*obj.t_i^3]; %  0
-                    
-                    
-                    obj.t_f = obj.t_i + 10; % delta T = 10 seconds
-                    
-                    d_f =         [1  obj.t_f   obj.t_f^2   obj.t_f^3    obj.t_f^4    obj.t_f^5]; % 2.5
-                    d_dot_f =     [0  1   2*obj.t_f   3*obj.t_f^2  4*obj.t_f^3  5*obj.t_f^4]; % 0
-                    d_ddot_f =    [0  0   2     6*obj.t_f    12*obj.t_f^2  20*obj.t_f^3]; % 0
-                    
-                    A = [d_i; d_dot_i; d_ddot_i; d_f; d_dot_f; d_ddot_f];
-                    
-                    B = [y; 0; 0; y-5; 0; 0];
-                    
-                    X = linsolve(A,B);
-                    
-                    obj.a0 = X(1);
-                    obj.a1 = X(2);
-                    obj.a2 = X(3);
-                    obj.a3 = X(4);
-                    obj.a4 = X(5);
-                    obj.a5 = X(6);
-                    
-                    
-                    
-                end
-                
                 if clock> obj.t_f
                     latSpeed=0;
                 else
