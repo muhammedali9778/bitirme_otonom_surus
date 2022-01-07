f3f95ab3e049361979ad9a8541c83b5b787d1ae7
Muhammet Ali ÖZTÜRK <muhammedali9778@gmail.com>
2022-01-07 15:43:37 +0300

    finalll

--- SeritDegisimi.m
+++ SeritDegisimi.m
@@ -109,39 +109,21 @@ classdef SeritDegisimi < matlab.System
         end
 
         function resetImpl(obj)
-            % Initialize / reset discrete-state properties
         end
 
         function out = getOutputSizeImpl(obj)
-            % Return size for each output port
             out = [1 1];
-
-            % Example: inherit size from first input port
-            % out = propagatedInputSize(obj,1);
         end
-
         function out = getOutputDataTypeImpl(obj)
-            % Return data type for each output port
             out = "double";
-
-            % Example: inherit data type from first input port
-            % out = propagatedInputDataType(obj,1);
         end
 
-        function out = isOutputComplexImpl(obj)
-            % Return true for each output port with complex data
+        function out = isOutputComplexImpl(obj)        
             out = false;
-
-            % Example: inherit complexity from first input port
-            % out = propagatedInputComplexity(obj,1);
         end
 
         function out = isOutputFixedSizeImpl(obj)
-            % Return true for each output port with fixed size
             out = true;
-
-            % Example: inherit fixed-size status from first input port
-            % out = propagatedInputFixedSize(obj,1);
         end
     end
 end
